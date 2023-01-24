d18bbcb1eca8fc10642a4ec0edce7527cd468a75
Rastislav Ružbacký <ruzbacky@yahoo.com>
2023-01-24 15:31:21 +0100

    add decorators for login

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
@@ -31,7 +34,8 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
+    
+    
+
 
 # Create your models here.
