91c5aacdba1741534b678ccb33b99740c8f50ce8
Rastislav Ružbacký <ruzbacky@yahoo.com>
2023-07-06 09:22:31 +0200

    Zmena nastavenia statických súborov

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL = '/'
