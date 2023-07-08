122394f4ec93b4228ee5fe8a6f66d09a9188e537
Rastislav Ružbacký <ruzbacky@yahoo.com>
2023-07-08 18:44:34 +0200

    Dalsie zmeny

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,15 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL = '/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static'),
     # '/var/www/static/',
-]
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
